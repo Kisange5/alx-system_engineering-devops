f51b6cc5b511f6d542dfdc7a9b2372a31899e989
Kisange5 <kisangelorraine.com>
2023-11-18 12:49:57 -0800

    0x1A. Application server

--- 0x1A-application_server/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# Using Puppet to create a file 
-file {'/tmp/school':
-ensure  => present,
-mode    => '0744',
-content => 'I love Puppet',
-owner   => 'www-data',
-group   => 'www-data',
-}
--- 0x1A-application_server/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# kill a process named killmenow
-exec {'kill process killmenow':
-    command  => 'pkill killmenow',
-    path     => '/usr/bin',
-    onlyif   => 'pgrep killmenow',
-    provider => shell,
-}
